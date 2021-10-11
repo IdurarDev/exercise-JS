2e16baca93a7d04b34fa248b8c60e9464d9ffd3b
Hamid Berkaine <hamiddev78@gmail.com>
2021-10-11 16:47:00 +0200

    add alert-prompt exersice

--- 03-alert-prompt-confirm/1-simple-page/script.js
+++ 03-alert-prompt-confirm/1-simple-page/script.js
@@ -2,3 +2,7 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+let myName = prompt('Quel est votre nom ?')
+
+alert(myName)
