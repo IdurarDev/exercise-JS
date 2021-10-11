4ab0f2ba341312557caf00537d2c9927ee269190
Hamid Berkaine <hamiddev78@gmail.com>
2021-10-11 16:45:43 +0200

    add types exersice

--- 02-types/1-string-quotes/script.js
+++ 02-types/1-string-quotes/script.js
@@ -2,3 +2,10 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+let nom = 'ilya'
+
+
+alert(`hello ${1}`) // hello 1
+alert(`hello ${"nom"}`) // hello nom
+alert(`hello ${nom}`) // ilya
