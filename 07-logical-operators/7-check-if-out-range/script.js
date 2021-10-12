e32c88af0189ab3b216d4f2a31efdf39dde7d164
Hamid Berkaine <hamiddev78@gmail.com>
2021-10-12 20:48:48 +0200

    modif file 7

--- 07-logical-operators/7-check-if-out-range/script.js
+++ 07-logical-operators/7-check-if-out-range/script.js
@@ -2,3 +2,18 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+let age = prompt("Entrez un age");
+
+if (age < 14 || age > 90) {
+  alert("ok")
+} else {
+  alert("pas ok")
+}
+
+if (!(age > 14 && age < 90)) {
+  alert("ok")
+} else {
+  alert("pas ok")
+}
+
