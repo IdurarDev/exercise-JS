a90bf55900239f64526189047f8c28ca38ac2783
Hamid Berkaine <hamiddev78@gmail.com>
2021-10-12 20:07:35 +0200

    modif file 6

--- 07-logical-operators/6-check-if-in-range/script.js
+++ 07-logical-operators/6-check-if-in-range/script.js
@@ -2,3 +2,11 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+let age = prompt("Entrez un age");
+
+if (age > 14 && age < 90) {
+      alert("Entre 14 et 90")
+} else {
+      alert(" - 14 ou + que 90")
+}
