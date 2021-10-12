a16d3d2d047e21d1f0b21eee70a18413d78d69f0
Hamid Berkaine <hamiddev78@gmail.com>
2021-10-12 09:37:00 +0200

    add comparaison exersice

--- 05-comparison/1-comparison-questions/script.js
+++ 05-comparison/1-comparison-questions/script.js
@@ -2,3 +2,11 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+5 > 4 // true
+"apple" > "pineapple" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // true
+null === +"\n0\n" // true
