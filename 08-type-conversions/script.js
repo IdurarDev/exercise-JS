665b81f7d930131ecc35a975ce818da7c8a0ca15
Hamid Berkaine <hamiddev78@gmail.com>
2021-10-13 10:43:02 +0200

    add exo conversion

--- 08-type-conversions/script.js
+++ 08-type-conversions/script.js
@@ -2,3 +2,26 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+// Conversion String
+let value = true;
+alert(typeof value);
+
+value = String(value);
+alert(typeof value)
+
+// Conversion Number
+let str = "123";
+alert(typeof str);
+
+str = +str
+alert(typeof str)
+str = Number(str)
+alert(typeof str)
+
+// Conversion Boolean
+let nb = 1;
+alert(typeof nb);
+
+nb = Boolean(nb)
+alert(typeof nb)
