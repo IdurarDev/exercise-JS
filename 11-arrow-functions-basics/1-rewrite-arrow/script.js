fcfb2ec3dc4e5f28ddfd040caa44c1420db21b11
Hamid Berkaine <hamiddev78@gmail.com>
2021-10-15 16:10:38 +0200

    add exo arrows-function

--- 11-arrow-functions-basics/1-rewrite-arrow/script.js
+++ 11-arrow-functions-basics/1-rewrite-arrow/script.js
@@ -2,3 +2,36 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+// expression de fonction 
+
+// let sayHi = function() {
+//   alert('saluuuut')
+// }; // préférable de mettre ' ; ' a la fin d'une expression de function
+// sayHi()
+
+const ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree ?",
+  function() { alert("You agreed.") ;},
+  function() { alert("You canceled the execution"); }
+);
+
+// Soluce formateur 
+/**
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
+ 
+**/
